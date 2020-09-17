3b9a0518a0670f2e2bf13f89d5a70081b7cc988c
serolkircil <serolkircil@gmail.com>
2020-09-17 22:35:43 +0300

    döngüler - sayının basamak sayısını ve tersini bulan döngüler

--- sample.js
+++ sample.js
@@ -6,11 +6,40 @@ function wl(a) {
 //sınıf çalışması
 
 
-let n = 4
 
-while(n--) {
-	wl(n)
+function getDigitCount(sayi) {
+
+	let basamak = 0
+
+	if (!sayi)
+		return 1
+
+	// return parseInt(Math.log10(Math.abs(sayi))) + 1
+
+	while (sayi) {
+		++basamak
+		sayi = parseInt(sayi / 10)
+	}
+	return basamak
 }
 
-wl(n)
+wl(getDigitCount(1234567))
 
+wl(getReverse(4321))
+
+function getReverse(sayi) {
+
+	let result = 0
+
+	if (sayi < 10)
+		return sayi
+
+	while(sayi)
+	{
+		result = result * 10 + sayi % 10
+		sayi = parseInt(sayi / 10)
+
+	}
+
+	return result
+}
