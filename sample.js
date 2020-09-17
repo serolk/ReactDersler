a391551d5d93f0c6947493c222b6c2bb0afc14be
serolkircil <serolkircil@gmail.com>
2020-09-17 22:08:57 +0300

    döngüler - while döngüsü değişik kullanımlar

--- sample.js
+++ sample.js
@@ -5,29 +5,12 @@ function wl(a) {
 ///////////////////////////////////////////////////////////////////////
 //sınıf çalışması
 
-findRoots(1,-3,-8)
-findRoots(1,1,1)
-findRoots(1,4,4)
-findRoots(3.4,-3,-9.8)
 
-function findRoots(a, b, c)
-{
-	if(delta > 0)
-	{
-		let delta = b * b - 4 * a * c
-		let sqrtDelta = Math.sqrt(delta)
-		let x1 = (-b + sqrtDelta) / (2 * a)
-		let x2 = (-b - sqrtDelta) / (2 * a)
-		wl(`x1 = ${x1} ve x2 = ${x2}`)
-	}
-	else if(!delta)
-	{
-		let x = -b / (2 * a)
-		wl(`x1 = x2 = ${x}`)
-	}
-	else
-	{
-		wl("Gerçek Kök")
-	}
+let n = 4
 
+while(n--) {
+	wl(n)
 }
+
+wl(n)
+
