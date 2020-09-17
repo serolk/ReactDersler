8faf523697d183e6dd344f606615e0df26f56c88
serolkircil <serolkircil@gmail.com>
2020-09-17 21:55:48 +0300

    sınıf çalışması - 2.dereceden denklemin köklerini bulmak

--- sample.js
+++ sample.js
@@ -5,7 +5,10 @@ function wl(a) {
 ///////////////////////////////////////////////////////////////////////
 //sınıf çalışması
 
-findRoots(1,10,25)
+findRoots(1,-3,-8)
+findRoots(1,1,1)
+findRoots(1,4,4)
+findRoots(3.4,-3,-9.8)
 
 function findRoots(a, b, c)
 {
@@ -18,11 +21,10 @@ function findRoots(a, b, c)
 		let x2 = (-b - sqrtDelta) / (2 * a)
 		wl(`x1 = ${x1} ve x2 = ${x2}`)
 	}
-	else if(delta === 0)
+	else if(!delta)
 	{
-		let x1 = -b / (2 * a)
-		let x2 = x1
-		wl(`x1 = ${x1} ve x2 = ${x2}`)
+		let x = -b / (2 * a)
+		wl(`x1 = x2 = ${x}`)
 	}
 	else
 	{
