e3e84086033cea02fe12e5d6de57a2a1f8eba912
serolkircil <serolkircil@gmail.com>
2020-09-22 21:52:45 +0300

    döngüler - armstrong sayısı mı?

--- sample.js
+++ sample.js
@@ -9,7 +9,7 @@ function isArmstrong(val)
 {
 	let sum = 0
 	let temp = val
-	while(temp > 0)
+	while(temp)
 	{
 		sum += Math.pow(parseInt(temp) % 10, 3)
 		temp = parseInt(temp) / 10
@@ -19,6 +19,32 @@ function isArmstrong(val)
 	return false
 }
 
+function getDigitCount(val)
+{
+	return val === 0 ? 1 : parseInt(Math.log10(Math.abs(val))) + 1
+}
+
+function getPowSum(val)
+{
+	let count = getDigitCount(val)
+	let sum = 0
+
+	while(val)
+	{
+		sum += Math.pow(val % 10, count)
+		val = parseInt(val / 10)
+	}
+	return sum
+}
+
+function isArmstrongHoca(val)
+{
+	if(val < 0)
+		return false
+	return getPowSum(val) === val
+}
+
+
 for(let i = 1; i < 1000000; i++)
-	if(isArmstrong(i))
-		wl(i + " : " + isArmstrong(i))
+	if(isArmstrongHoca(i))
+		wl(i + " : " + isArmstrongHoca(i))
