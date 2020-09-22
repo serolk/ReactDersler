6ab0ea73910b7f5f6b40299bc2cd7c3215616091
serolkircil <serolkircil@gmail.com>
2020-09-22 19:57:33 +0300

    döngüler - gözdesiz for

--- sample.js
+++ sample.js
@@ -3,43 +3,11 @@ function wl(a) {
 }
 
 ///////////////////////////////////////////////////////////////////////
-//sınıf çalışması
+//gövdesiz for
 
-
-
-function getDigitCount(sayi) {
-
-	let basamak = 0
-
-	if (!sayi)
-		return 1
-
-	// return parseInt(Math.log10(Math.abs(sayi))) + 1
-
-	while (sayi) {
-		++basamak
-		sayi = parseInt(sayi / 10)
-	}
-	return basamak
+function  main() {
+	for(let i = 0, k = 10; i < 10 && k > 0; wl(`i = ${i} ve k = ${k}`), ++i, k-= 2);
 }
 
-wl(getDigitCount(1234567))
 
-wl(getReverse(4321))
-
-function getReverse(sayi) {
-
-	let result = 0
-
-	if (sayi < 10)
-		return sayi
-
-	while(sayi)
-	{
-		result = result * 10 + sayi % 10
-		sayi = parseInt(sayi / 10)
-
-	}
-
-	return result
-}
+main()
