3966f7a04ee2dbc03fb17a7be752c0ba0061b7fc
serolkircil <serolkircil@gmail.com>
2020-09-22 21:38:08 +0300

    döngüler - armstrong sayısı mı?

--- sample.js
+++ sample.js
@@ -3,53 +3,7 @@ function wl(a) {
 }
 
 ///////////////////////////////////////////////////////////////////////
-//gövdesiz for
-
-function isPrime(val)
-{
-	if(val <= 1)
-		return false
-
-	if(val % 2 === 0)
-		return val === 2
-
-	if(val % 3 === 0)
-		return val === 3
-
-	if(val % 5 === 0)
-		return val === 5
-
-	if(val % 7 === 0)
-		return val === 7
-
-	sqrt = Math.sqrt(val)
-	if(sqrt === parseInt(Math.sqrt(val)))
-		return false
-
-	for(let i = 11; i <= sqrt; i += 2) {
-		if(num % i === 0)
-			return false;
-	}
-
-	return true;
-
-}
-
-function getPrime(n)
-{
-	let count = 0
-	let val = 2
-
-	while(1) {
-		if (isPrime(val))
-			++count
-
-		if(count == n)
-			return val
-
-		++val
-	}
-}
+//
 
 function isArmstrong(val)
 {
@@ -61,9 +15,10 @@ function isArmstrong(val)
 		temp = parseInt(temp) / 10
 	}
 	if(sum === val)
-		wl("Armstrong")
-	else
-		wl("Not")
+		return true
+	return false
 }
 
-isArmstrong(153)
+for(let i = 1; i < 1000000; i++)
+	if(isArmstrong(i))
+		wl(i + " : " + isArmstrong(i))
