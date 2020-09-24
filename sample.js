af8a2fba809d388c02e12b30971e632c07ba92e1
serolkircil <serolkircil@gmail.com>
2020-09-24 22:17:51 +0300

    diziler - fonksiyonlar giriş

--- sample.js
+++ sample.js
@@ -2,34 +2,64 @@ function wl(a) {
 	console.log(a)
 }
 
-///////////////////////////////////////////////////////////////////////
-//paremetresi ile aldığı bir sayıdan büyük en küçük fibonacii sayısını dön
-
-
-function nextFibonacci(val)
-{
-	let prev1 = 1, prev2 = 0, result = 0
-
-	if(val < 0)
-		return 0
-
-	do
-	{
-		result = prev1 + prev2
-		prev2 = prev1
-		prev1 = result
-
-	} while(val >= result )
-
-	return result
+function write(a){
+	process.stdout.write(a)
 }
+
+function writeline(a){
+	write(a + "\r\n")
+}
+
+///////////////////////////////////////////////////////////////////////
 
 function main() {
 
-		wl(nextFibonacci(4))
+	// let names = ["ali", "veli", "kerime"]
+	// let emptyArray = []
+	// let sameAsEmptyArray = new Array()
+	// let a = new Array(10)
+
+	let arr = [1, 2, 3, 4, 5, 6, 7]
+	displayArr(getReverse(arr))
+
 
 }
 
+//Function in function applicapable:
+
+// let foo = function (){
+//
+// 	writeline("foo")
+// 	let bar = function(){
+// 		writeline("bar")
+// 	}
+//
+// 	bar()
+// }
+
+
+function getReverse(arr){
+
+	let halfLen = arr.length / 2
+	for(let i = 0; i < halfLen; ++i)
+	{
+		let temp = arr[i]
+		arr[i] = arr[arr.length - 1 - i]
+		arr[arr.length - 1 - i] = temp
+	}
+
+	return arr
+}
+
+function displayArr(arr){
+
+	for (let x of arr){
+		writeline(x)
+	}
+}
+
 main()
+
+
 
 
